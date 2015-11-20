5d624fd542400f1a61e3bdaf849c9a2fbbb2edd2
Matt Leach <matt.leach25@gmail.com>
2015-11-19 16:10:36 -0800

    clustering file with outlined functions

---
+++ clustering/__init__.py
@@ -0,0 +1,33 @@
+# Clustering file
+
+
+def pca(X, num_clusters):
+    '''
+    Principal Component Analysis on X for response y
+    Returns array of cluster groups
+    '''
+    return []
+
+
+def kmeans(X, num_clusters):
+    '''
+    K-Means on X for response y
+    Returns array of cluster groups
+    '''
+    return []
+
+
+def hiearchicalr(X, num_clusters):
+    '''
+    Hierarchical Clustering on X for response y
+    Returns array of cluster groups
+    '''
+    return []
+
+
+def spectral(X, num_clusters):
+    '''
+    Spectral Clustering on X for response y
+    Returns array of cluster groups
+    '''
+    return []
