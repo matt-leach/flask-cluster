a7888b318412d231e8a2e5be2db91616a071586d
Matt Leach <matt.leach25@gmail.com>
2015-11-28 12:01:17 -0800

    /data endpoint with uploading file, reading builtin

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,6 @@
 from app import app
 import settings
-from data import convert_csv
+from data import convert_csv, convert_str
 
 from flask import render_template, jsonify, request, session
 import pandas as pd
@@ -22,6 +22,7 @@ def cluster():
         methods = ','.join(settings.CLUSTER_METHODS.keys())
         error = "parameter 'method' must be one of the following: {}".format(methods)
         return jsonify({'error': error})
+
     try:
         num_clusters = int(request.args.get('clusters'))
         if num_clusters < 1:
@@ -44,3 +45,23 @@ def cluster():
     if return_X_data:
         data['variables'] = {var: X[var].tolist() for var in X}
     return jsonify(data)
+
+
+@app.route('/data', methods=['POST'])
+def load_data():
+    session['data'] = None
+    try:
+        file_name = json.loads(request.data)['builtin']
+    except Exception as e:
+        print e
+        file_name = None
+    if file_name:
+        try:
+            session['data'] = convert_csv('data/{}.csv'.format(file_name)).to_json()
+        except IOError:
+            return jsonify({'error': "No built in data file '{}'.".format(file_name)})
+    else:
+        # TODO: check works with javascript shizzle
+        session['data'] = convert_str(request.data).to_json()
+
+    return ""
