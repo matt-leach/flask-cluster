a477c0ae2b6c3e4e77dda09ac1cd0c7871404b98
Matt Leach <matt.leach25@gmail.com>
2015-11-28 11:23:39 -0800

    adding parameter checking

--- settings.py
+++ settings.py
@@ -4,3 +4,5 @@ CLUSTER_METHODS = {'pca': pca,
                    'kmeans': kmeans,
                    'hierarchical': hierarchical,
                    'spectral': spectral}
+
+DEFAULT_DATA = 'data/cereal.csv'
--- views/__init__.py
+++ views/__init__.py
@@ -2,9 +2,10 @@ from app import app
 import settings
 from data import convert_csv
 
-from flask import render_template, jsonify, request
+from flask import render_template, jsonify, request, session
 import pandas as pd
 import numpy as np
+import json
 
 
 @app.route('/')
@@ -14,15 +15,29 @@ def home():
 
 @app.route('/cluster')
 def cluster():
-    method_name = request.args.get('method')
-    method = settings.CLUSTER_METHODS[method_name]
-    num_clusters = int(request.args.get('clusters'))
+    try:
+        method_name = request.args.get('method')
+        method = settings.CLUSTER_METHODS[method_name]
+    except KeyError:
+        methods = ','.join(settings.CLUSTER_METHODS.keys())
+        error = "parameter 'method' must be one of the following: {}".format(methods)
+        return jsonify({'error': error})
+    try:
+        num_clusters = int(request.args.get('clusters'))
+        if num_clusters < 1:
+            raise TypeError
+    except (TypeError, ValueError):
+        error = "parameter 'clusters' must be an integer number of clusters > 0"
+        return jsonify({'error': error})
+
     return_X_data = bool(request.args.get('data'))
 
-    # TODO: error handling
-
-    X = convert_csv('static/cereal.csv')
-
+    # Get data - either from session or from DEFAULT_DATA location
+    if not session.get('data'):
+        X = convert_csv(settings.DEFAULT_DATA)
+        session['data'] = X.to_json()  # Store in session as json
+    else:
+        X = pd.read_json(session['data'])
     cluster_vals = method(X, num_clusters)
 
     data = {'clusters': {num_clusters: cluster_vals.tolist()}}
