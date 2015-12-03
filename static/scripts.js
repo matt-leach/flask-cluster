392c27ebc7ed2fc8e6a8c767db91373d175a8164
alexemorris <alexemorris@icloud.com>
2015-12-03 20:38:41 +0000

    more updates

--- static/scripts.js
+++ static/scripts.js
@@ -7,14 +7,12 @@ noUiSlider.create(sliderVert, {
 	orientation: "vertical",
     range: {
         min: 0,
-        max: 4
+        max: 50
     },
     direction: 'rtl',
     pips: {
-        mode: 'values',
-        values: [0,1, 2, 3, 4],
-        density: 40,
-        stepped: true
+		mode: 'range',
+		density: 1
     }
 });
 
@@ -47,6 +45,7 @@ var bubbleChartData = {
     animation: {
         duration: 10000
     },
+    
     datasets: [{
         label: "My Second dataset",
         backgroundColor: randomColor(),
@@ -148,7 +147,9 @@ var bubbleChartData = {
 };
 
 function resizeApplicationControls() {
-	if ($(window).width() > 600) {
+	$('.plotting-area').css('height', $('#canvas').outerHeight() + 80)
+	
+	if ($(window).width() > 768) {
 		$('.application-controls').css('height', $('.plotting-area').innerHeight())
 	} else {
 		$('.application-controls').css('height', 'auto')
@@ -174,6 +175,19 @@ window.onload = function() {
     updateLegend();
 	
 	resizeApplicationControls()
+	
+	
+	$('.info-button').click(function(event) {
+		div_to_show = $(this).attr("show_div")
+		console.log(div_to_show)
+		$(div_to_show).fadeIn()
+		$("#dimmer").fadeIn()
+	})
+	
+	$('.info i').click(function() {
+		$('.info-text-wrapper').fadeOut()
+		$("#dimmer").fadeOut()
+	})
 	
 
 };
